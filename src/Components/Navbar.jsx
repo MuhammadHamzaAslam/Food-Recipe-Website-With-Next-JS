468256fd9158487af56ece8e9bf8b46b46f1a687
Hamza <153912992+MuhammadHamzaAslam@users.noreply.github.com>
2024-10-09 16:35:12 +0500

    updateing issue

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from 'react';
 import Link from 'next/link';
-
+import '../app/globals.css'
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
-'use client'
 import Navbar from "@/Components/Navbar";
 import Image from "next/image";
-
+import './globals.css'
 export default function Home() {
   return (
     <Navbar/>
